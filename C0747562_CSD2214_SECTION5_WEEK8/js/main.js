90fc58efade42ce744aa76ae49667f6871cf9c88
KARAN BANWAIT <karanbanwaitsaini@gmail.com>
2019-02-28 12:01:42 -0500

    looping challange

--- C0747562_CSD2214_SECTION5_WEEK8/js/main.js
+++ C0747562_CSD2214_SECTION5_WEEK8/js/main.js
@@ -1,1 +1,39 @@
 ﻿// my code here!
+
+//const circle = {
+//    radius: 1,
+//    location: {
+//        x: 1,
+//        y: 1
+//    },
+
+//    draw: function () {
+//        console.log('draw');
+//    }
+//};
+//circle.draw();
+
+
+//factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw circle with factory function and radius:' + radius);
+        }
+    };
+}
+
+const circle = createCircle(1);
+circle.draw();
+
+function Circle(radius) {
+    console.log('this',this)
+    this.radius = radius;
+    this.draw =function () {
+            console.log('draw circle with constructor and radius:' + radius);
+   }
+}
+
+const anotherCircle = new Circle(1);
+anotherCircle.draw();
